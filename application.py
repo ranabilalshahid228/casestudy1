760a465d8cde21edd4e02769e8c68183623fa9b3
ranabilalshahid228 <56960509+ranabilalshahid228@users.noreply.github.com>
2024-02-18 01:34:13 +0100

    Update application.py

--- application.py
+++ application.py
@@ -93,3 +93,37 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+    import streamlit as st
+
+# Custom CSS to position and style images
+st.markdown(
+    """
+    <style>
+    .image-container {
+        position: fixed; /* Change from relative to fixed */
+        top: 10px; /* Align to the top of the viewport */
+        left: 10px; /* Align to the right of the viewport */
+        margin: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+    }
+    .image-container img {
+        width: 200px; /* Set the width as per your requirement */
+        height: auto;
+        margin-bottom: 5px; /* Adjust spacing between images, if needed */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Display images in a container
+st.markdown(
+    """
+    <div class="image-container">
+        <img src="https://i.pinimg.com/736x/06/de/62/06de624722bcde1117f99d65d5530f43.jpg" alt="QR code">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
