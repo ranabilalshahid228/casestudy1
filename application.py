c0963da7e5b4e7de153dab06d559ab123e947885
ranabilalshahid228 <56960509+ranabilalshahid228@users.noreply.github.com>
2024-02-18 02:57:11 +0100

    Update application.py

--- application.py
+++ application.py
@@ -60,8 +60,9 @@ if st.button('Predict'):
 
     # Display the prediction
     st.write(f'Predicted Total Production for Selected Department and at selected Workers: {prediction}')
-   # Display a caption with black text using st.markdown
-st.caption( "R-squared (Train): 0.9135524926797545 and Normalized MSE:0.0036")
+# Display a caption in Streamlit
+st.caption("R-squared (Train): 0.9135524926797545 and Normalized MSE:0.0036")
+
 
 
     import streamlit as st
